b57427026a46ed0464a9b5869a1a7aad9b85abe2
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-15 16:34:35 +0530

    refactored code to store results in dictionary

--- sorting_arithmatic_computation.sh
+++ sorting_arithmatic_computation.sh
@@ -3,7 +3,8 @@ declare -A computation
 read -p "Enter value of a" a
 read -p "Enter value of b" b
 read -p "Enter value of c" c
-computation=result=$(($a+$b*$c))
-computation=result1=$(($a*$b+$c))
-computation=result2=$(($c+$a/$b))
-computation=result3=$(($a%$b+$c))
+computation[result]="$(($a+$b*$c))"
+computation[result1]="$(($a*$b+$c))"
+computation[result2]="$(($c+$a/$b))"
+computation[result3]="$(($a%$b+$c))"
+echo ${computation[@]}
