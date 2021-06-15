5c055c4aef3ca1e15cb67da68c767a840e9fa0aa
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-15 16:11:10 +0530

    refactored code to store values from the dictionary into the array

--- sorting_arithmatic_computation.sh
+++ sorting_arithmatic_computation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 declare -A computation
+declare -a computations
 read -p "Enter value of a" a
 read -p "Enter value of b" b
 read -p "Enter value of c" c
@@ -7,3 +8,4 @@ computation=result=$(($a+$b*$c))
 computation=result1=$(($a*$b+$c))
 computation=result2=$(($c+$a/$b))
 computation=result3=$(($a%$b+$c))
+computations=${computation[@]}
