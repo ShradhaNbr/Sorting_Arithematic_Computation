8dfd2a6f4bf7d338c603d086d254cc1ad083ede6
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-15 18:05:50 +0530

    adding values fom dictionary to array

--- sorting_arithmatic_computation.sh
+++ sorting_arithmatic_computation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 declare -A computation
 declare -a comp
 read -p "Enter value of a" a
@@ -8,7 +8,9 @@ computation[result]="$(($a+$b*$c))"
 computation[result1]="$(($a*$b+$c))"
 computation[result2]="$(($c+$a/$b))"
 computation[result3]="$(($a%$b+$c))"
-i=0
-comp[$i]=${computation[@]}
+comp[0]=$((computation[result]))
+comp[1]=$((computation[result1]))
+comp[2]=$((computation[result2]))
+comp[3]=$((computation[result3]))
 echo ${comp[@]}
-echo ${#comp[@]}
+
