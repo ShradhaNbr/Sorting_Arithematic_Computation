d45985012d4fd8573664e4640a3893efb1ea4ab5
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-15 17:22:27 +0530

    adding values from dictionary into array

--- sorting_arithmatic_computation.sh
+++ sorting_arithmatic_computation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 declare -A computation
+declare -a comp
 read -p "Enter value of a" a
 read -p "Enter value of b" b
 read -p "Enter value of c" c
@@ -7,5 +8,7 @@ computation[result]="$(($a+$b*$c))"
 computation[result1]="$(($a*$b+$c))"
 computation[result2]="$(($c+$a/$b))"
 computation[result3]="$(($a%$b+$c))"
-echo ${computation[@]
-
+i=0
+comp[$i]=${computation[@]}
+echo ${comp[@]}
+echo ${#comp[@]}
