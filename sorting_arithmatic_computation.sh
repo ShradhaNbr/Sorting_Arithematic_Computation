0228a90a191cdb7a8f78e8458c62176e1d97260e
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-15 18:14:39 +0530

    Refactor the code to sort computation in descending order

--- sorting_arithmatic_computation.sh
+++ sorting_arithmatic_computation.sh
@@ -1,24 +1,28 @@
-#!/bin/bash -x
+#!/bin/bash 
 declare -A computation
-declare -a computations
+declare -a comp
 read -p "Enter value of a" a
 read -p "Enter value of b" b
 read -p "Enter value of c" c
-computation=result=$(($a+$b*$c))
-computation=result1=$(($a*$b+$c))
-computation=result2=$(($c+$a/$b))
-computation=result3=$(($a%$b+$c))
-computations=${computation[@]}
-for((i=0;i<5;++i))
+computation[result]="$(($a+$b*$c))"
+computation[result1]="$(($a*$b+$c))"
+computation[result2]="$(($c+$a/$b))"
+computation[result3]="$(($a%$b+$c))"
+comp[0]=$((computation[result]))
+comp[1]=$((computation[result1]))
+comp[2]=$((computation[result2]))
+comp[3]=$((computation[result3]))
+echo ${comp[@]}
+for((i=0;i<4;++i))
 do
-        for((j=(($i+1));j<10;++j))
+        for((j=(($i+1));j<4;++j))
         do
-        if [ ${computations[$i]} -lt ${computations[$j]} ]
+        if [ ${comp[$i]} -lt ${comp[$j]} ]
         then
-        temp=${computations[$i]}
-        computations[i]=${computations[$j]}
-        computations[j]=$temp
+        temp=${comp[$i]}
+        comp[i]=${comp[$j]}
+        comp[j]=$temp
         fi
         done
 done
-echo "Numbers in descending order are ${computations[@]}"
+echo "Numbers in descending order are ${comp[@]}"
