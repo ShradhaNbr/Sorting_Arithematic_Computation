c59144b33e6fa092b1d1518fe308de2e5b005a53
ShradhaNbr <sradha.nambiar@gmail.com>
2021-06-15 16:27:50 +0530

    refactored code to store results in dictionary

--- sorting_arithmatic_computation.sh
+++ sorting_arithmatic_computation.sh
@@ -9,3 +9,16 @@ computation=result1=$(($a*$b+$c))
 computation=result2=$(($c+$a/$b))
 computation=result3=$(($a%$b+$c))
 computations=${computation[@]}
+for((i=0;i<5;++i))
+do
+        for((j=(($i+1));j<10;++j))
+        do
+        if [ ${computations[$i]} -lt ${computations[$j]} ]
+        then
+        temp=${computations[$i]}
+        computations[i]=${computations[$j]}
+        computations[j]=$temp
+        fi
+        done
+done
+echo "Numbers in descending order are ${computations[@]}"
